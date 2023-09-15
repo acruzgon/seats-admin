d5065c2e43421ce0bf29ea1421ed38c271989d5c
Adriana Cruz <acruzgon@redhat.com>
2023-09-15 12:52:57 -0600

    checking cypress workflow

--- cypress/e2e/lightspeed-org-admin.cy.ts
+++ cypress/e2e/lightspeed-org-admin.cy.ts
@@ -4,7 +4,7 @@ describe('Navigate to the Prod Seats Administration page as lightspeed-org-admin
   });
 
   it('passes', () => {
-    cy.get('.pf-c-title', { timeout: 30000 }).should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
+    cy.get('.pf-c-title', { timeout: 60000 }).should('include.text', 'Ansible Lightspeed with IBM watsonx Code Assistant');
     cy.get('.pf-l-level > div > p').should('include.text', 'This group contains all users assigned seats within your organization.');
   })
 })
